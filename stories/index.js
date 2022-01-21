7e2545c3c8024d6c2b8b8c642a1ced8ad25218de
Reflekshn <reflekshn@hotmail.com>
2022-01-20 23:23:40 -0500

    Add story test for the Status Component

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 storiesOf("Button", module)
   .addParameters({
@@ -165,4 +166,8 @@ storiesOf("Appointment", module)
       message="Delete the appointment?"
       onConfirm={action("onConfirm")}
       onCancel={action("onCancel")} />
+  ))
+  .add("Status", () => (
+    <Status
+      message="Deleting" />
   ));
