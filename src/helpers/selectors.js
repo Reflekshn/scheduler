fcacf7160e2181c690402a07e7af0c76afa80664
Reflekshn <reflekshn@hotmail.com>
2022-01-23 14:49:27 -0500

    Implement getInterview helper function to retrieve an individual interview, along with a JEST test to ensure functionality

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -13,3 +13,16 @@ export function getAppointmentsForDay(state, day) {
 
   return appointmentsForDay;
 }
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+
+  const theInterview = {
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer]
+  };
+
+  return theInterview;
+}
