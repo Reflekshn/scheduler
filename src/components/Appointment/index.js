e91c6f1d0f3b30afbedc8572af0ccc59acd81a11
Reflekshn <reflekshn@hotmail.com>
2022-01-23 14:52:57 -0500

    Remove unused formatText function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,13 +6,6 @@ import Empty from './Empty';
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
-  const formatText = () => {
-    if (props.time) {
-      return `Appointment at ${props.time}`;
-    } else {
-      return `No Appointments`;
-    }
-  }
   return (
     <article className='appointment'>
       <Header time={props.time} />
