acc83b7868cd0c028497feee6715f392deb17300
Reflekshn <reflekshn@hotmail.com>
2022-01-24 18:15:23 -0500

    Remove commented out example code

--- src/components/hooks/useVisualMode.js
+++ src/components/hooks/useVisualMode.js
@@ -10,7 +10,7 @@ export default function useVisualMode(initialMode) {
     if (replace) {
       newHistory.pop();
     }
-    
+
     newHistory.push(newMode);
 
     setHistory(newHistory);
@@ -34,23 +34,3 @@ export default function useVisualMode(initialMode) {
     back
   };
 }
-
-
-// function useControlledInput(initial) {
-//   const [value, setValue] = useState(initial);
-
-//   return {
-//     value,
-//     onChange: (event) => setValue(event.target.value)
-//   };
-// }
-
-
-// import { useEffect } from "react";
-
-// export default function useDebounce(operation, ms) {
-//   useEffect(() => {
-//     const handle = setTimeout(operation, ms);
-//     return () => clearTimeout(handle);
-//   }, [operation, ms]);
-// }
