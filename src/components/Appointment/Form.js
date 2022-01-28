a8ac23918ba44e658ddc11cc6b641a2702f483cf
Reflekshn <reflekshn@hotmail.com>
2022-01-28 04:46:37 -0500

    Create validation function for newly created tests, along with relevant error messages.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,8 @@ export default function Form(props) {
   // Set initial values for student and interviewer state variables
   const [student, setStudent] = useState(props.student || '');
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [studentError, setStudentError] = useState('');
+  const [interviewerError, setInterviewerError] = useState('');
 
   // Resets the state variables back to empty values
   const reset = () => {
@@ -19,6 +21,23 @@ export default function Form(props) {
     props.onCancel();
   };
 
+  // Validate that a student name has been entered and an interviewer has been selected
+  const validate = () => {
+    if (student === '') {
+      setStudentError('Student name cannot be blank');
+      return;
+    }
+
+    if (!interviewer) {
+      setInterviewerError('Please select an interviewer');
+      return;
+    }
+
+    setStudentError('');
+    setInterviewerError('');
+    props.onSave(student, interviewer);
+  };
+
   // Render the Form Component to book a new interview
   return (
     <main className="appointment__card appointment__card--create">
@@ -28,23 +47,30 @@ export default function Form(props) {
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
-            placeholder="Enter student name"
+            placeholder="Enter Student Name"
             value={student}
             onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{studentError}</section>
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
           onChange={setInterviewer}
         />
+        <section className="appointment__validation">
+          {interviewerError}
+        </section>
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={cancel} danger>
             Cancel
           </Button>
-          <Button onClick={() => props.onSave(student, interviewer)} confirm>Save</Button>
+          <Button onClick={() => validate()} confirm>
+            Save
+          </Button>
         </section>
       </section>
     </main>
