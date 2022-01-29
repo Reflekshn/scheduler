470f6a9a1ada80fd25adf7908d5e1b31a3f3ed35
Reflekshn <reflekshn@hotmail.com>
2022-01-29 02:48:41 -0500

    Add data attribute to the li element for testing

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import classNames from 'classnames';
-import 'components/DayListItem.scss'
+import 'components/DayListItem.scss';
 
 export default function DayListItem(props) {
-  const {name, spots, selected, setDay} = props;
+  const { name, spots, selected, setDay } = props;
   const dayListItemClass = classNames({
     'day-list__item': true,
     'day-list__item--selected': selected,
-    'day-list__item--full': !spots
+    'day-list__item--full': !spots,
   });
 
   const formatSpots = () => {
@@ -26,9 +26,14 @@ export default function DayListItem(props) {
   };
 
   return (
-    <li className={dayListItemClass} onClick={() => setDay(name)} selected={selected}>
-      <h2 className='text--regular'>{name}</h2>
-      <h3 className='text--light'>{formatSpots()}</h3>
+    <li
+      className={dayListItemClass}
+      onClick={() => setDay(name)}
+      selected={selected}
+      data-testid="day"
+    >
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
